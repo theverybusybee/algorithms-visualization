052c62a7ddb2f67ff5309407c661c2aae6d22b54
theverybusybee <veronika.antonchik@gmail.com>
2023-04-07 23:12:11 +0300

    constants for tests added

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,9 +6,30 @@ export const testSortingCard = '[data-testid="sorting-card"]';
 export const testStackCard = '[data-testid="stack-card"]';
 export const testQueueCard = '[data-testid="queue-card"]';
 export const testListCard = '[data-testid="list-card"]';
+
 export const testInput = '[data-testid="input"]';
-export const testButton = '[data-testid="button"]';
+
+export const submitButton = '[data-testid="submit-button"]';
 export const addButton = '[data-testid="add-button"]';
 export const removeButton = '[data-testid="remove-button"]';
 export const clearButton = '[data-testid="clear-button"]';
+export const addToHeadButton = '[data-testid="add-to-head-button"]';
+export const addToTailButton = '[data-testid="add-to-tail-button"]';
+export const removeFromHeadButton = '[data-testid="remove-from-head-button"]';
+export const removeFromTailButton = '[data-testid="remove-from-tail-button"]';
+export const addElementByIndexButton = '[data-testid="add-element-by-index"]';
+export const removeElementByIndexButton =
+  '[data-testid="remove-element-by-index"]';
+
+export const circleContent = '[data-testid="circle-content"]';
 export const testCircle = '[data-testid="circle"]';
+export const circleIndex = '[data-testid="circleIndex"]';
+
+export const circlePointer = '[data-testid="circle-pointer"]';
+export const circlePointerContent = '[data-testid="circle-pointer-content"]';
+
+export const testHead = '[data-testid="head"]';
+export const testTail = '[data-testid="tail"]';
+
+export const inputForValue = '[data-testid="input-for-value"]';
+export const inputForIndex = '[data-testid="input-for-index"]';
