fe3f5b5de8d2ff5e536712eaa6494579d4c7856e
theverybusybee <v_antonchik@mail.ru>
2023-12-09 00:06:59 +0300

    react version updated

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const baseURL = "https://theverybusybee.github.io/algorithms-visualization/";
+export const baseURL = "http://localhost:3000";
 
 export const testStringCard = '[data-testid="string-card"]';
 export const testFibonacciCard = "[data-testid='fibonacci-card']";
